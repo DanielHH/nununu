145e953854611c8eac33db1168636549f62fa3db
Richard Almgren Friberg <rical803@student.liu.se>
2018-11-17 22:46:10 +0100

    Draggable flatlist added to EditMenuScreen

--- NununuCompany/screens/EditMenuScreen.js
+++ NununuCompany/screens/EditMenuScreen.js
@@ -1,15 +1,60 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TouchableOpacity} from 'react-native';
+import DraggableFlatList from 'react-native-draggable-flatlist'
 
 export default class EditMenuScreen extends React.Component {
+  state = {
+    data: [...Array(20)].map((d, index) => ({
+      key: `item-${index}`,
+      name: "Hamburgare",
+      price: index + 10,
+      backgroundColor: `rgb(${Math.floor(Math.random() * 255)}, ${index * 5}, ${132})`,
+    }))
+  }
+
+  renderItem = ({ item, index, move, moveEnd, isActive }) => {
+    return (
+      <TouchableOpacity
+        style={{
+          height: 100,
+          backgroundColor: isActive ? 'blue' : item.backgroundColor,
+          alignItems: 'center',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}
+        onLongPress={move}
+        onPressOut={moveEnd}
+      >
+        <Text style={{
+          fontWeight: 'bold',
+          color: 'white',
+          fontSize: 32,
+        }}>
+          {item.name}
+        </Text>
+        <Text style={{
+          fontWeight: 'bold',
+          color: 'black',
+          fontSize: 32,
+          backgroundColor: 'green',
+        }}>
+          {item.price}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
 
   render() {
     return (
-      <View>
-        <Text>
-          meny
-        </Text>
+      <View style={{ flex: 1 }}>
+        <DraggableFlatList
+          data={this.state.data}
+          renderItem={this.renderItem}
+          keyExtractor={(item, index) => `draggable-item-${item.key}`}
+          scrollPercent={5}
+          onMoveEnd={({ data }) => this.setState({ data })}
+        />
       </View>
-    );
+    )
   }
 }
