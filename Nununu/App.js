3ac19776802135614eac34ad42b63c4d5e5d3f42
Richard Almgren Friberg <rical803@student.liu.se>
2018-11-21 16:03:05 +0100

    Fixed errors, now runnable.

--- Nununu/App.js
+++ Nununu/App.js
@@ -2,12 +2,16 @@
 
 import React from "react";
 import { View, Text } from "react-native";
-import { createStackNavigator} from "react-navigation";
+import { createStackNavigator, createAppContainer} from "react-navigation";
 import HomeScreen from "./screens/HomeScreen"
 
 const AppNavigator = createStackNavigator({
   Home: {
-    screen: HomeScreen
-});
+    screen: HomeScreen,
+    navigationOptions: ({ navigation }) => ({
+      headerTitle: 'Sukaldary',
+    }),
+  },
+})
 
-export default AppNavigator;
+export default createAppContainer(AppNavigator)
