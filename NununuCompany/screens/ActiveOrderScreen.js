00d2e7a58fc1426405c659ea8d816372e14ce2e2
Daniel Herzegh <danhe178@student.liu.se>
2018-11-21 16:49:58 +0100

    add example flatlist for active orders

--- NununuCompany/screens/ActiveOrderScreen.js
+++ NununuCompany/screens/ActiveOrderScreen.js
@@ -1,14 +1,58 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, FlatList, TouchableOpacity} from 'react-native'
 
 export default class ActiveOrderScreen extends React.Component {
 
+state = {
+  data: [...Array(8)].map((d, index) => ({
+    key: `item-${index}`,
+    content: String(index + 1) + ' st Hamburgare, ' + String(Math.max(index-14, 1)) + 'st Coca-cola',
+    orderNumber: '#' + String(index + 1),
+    backgroundColor: `rgb(${index * 5}, ${Math.floor(Math.random() * 255)}, ${132})`,
+  })),
+}
+
+renderItem = ({ item, index, isActive }) => {
+  return (
+    <TouchableOpacity
+      style={{
+        height: 100,
+        backgroundColor: isActive ? 'blue' : item.backgroundColor,
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}
+    >
+    <Text style={{
+      fontWeight: 'bold',
+      color: 'black',
+      fontSize: 32,
+      backgroundColor: 'yellow',
+    }}>
+      {item.orderNumber}
+    </Text>
+
+      <Text style={{
+        fontWeight: 'bold',
+        color: 'white',
+        fontSize: 32,
+      }}>
+        {item.content}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
   render() {
     return (
-      <View>
-        <Text>
-          Lista Aktiva Ordrar
-        </Text>
+      <View style={{flex : 1 }}>
+      <FlatList
+        data={this.state.data}
+        extraData={this.state}
+        renderItem={this.renderItem}
+        scrollPercent={5}
+        onMoveEnd={({ data }) => this.setState({ data })}
+        />
       </View>
     )
   }
