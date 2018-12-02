40b92a5e76c683d257f192751d60a675e12f6448
Richard Almgren Friberg <rical803@student.liu.se>
2018-12-02 14:53:56 +0100

    Created a faethersSetup.js file where the connection to the backend is setup. Initialization of connection is done in app.js.

--- NununuCompany/App.js
+++ NununuCompany/App.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react'
 import {View} from 'react-native'
 import Navigation from './navigation'
+import { connection } from './feathersSetup'
 
 type Props = {};
 
 export default class App extends Component<Props> {
+
+  constructor(props) {
+    super(props)
+    connection.setupConnection()
+  }
+
   render() {
     return (
       <View style={{flex: 1}}>
