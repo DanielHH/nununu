8c55d113fba266a1bea046f25bf083968db567a1
Daniel Herzegh <danhe178@student.liu.se>
2018-12-03 12:16:04 +0100

    add redux persist

--- NununuCompany/App.js
+++ NununuCompany/App.js
@@ -1,15 +1,26 @@
 import React, {Component} from 'react'
 import {View} from 'react-native'
 import Navigation from './navigation'
+import { PersistGate } from 'redux-persist/integration/react'
+import testExport from './configureStore'
+import { Provider } from 'react-redux'
 
 type Props = {};
 
 export default class App extends Component<Props> {
+  constructor (props) {
+    super(props)
+    this.conf = testExport()
+  }
   render() {
     return (
-      <View style={{flex: 1}}>
-        <Navigation/>
-      </View>
+      <Provider store={this.conf.store}>
+        <PersistGate loading={null} persistor={this.conf.persistor}>
+          <View style={{flex: 1}}>
+            <Navigation/>
+          </View>
+        </PersistGate>
+      </Provider>
     )
   }
 }
---
+++ NununuCompany/configureStore.js
@@ -0,0 +1,18 @@
+import { createStore } from 'redux'
+import { persistStore, persistCombineReducers } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
+
+import { reducers } from './redux/reducers'
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistCombineReducers(persistConfig, reducers)
+
+export default function testExport () {
+  let store = createStore(persistedReducer)
+  let persistor = persistStore(store)
+  return { store, persistor }
+}
