6e528aa2f9f49c1b08f9e67cc9b21318c766d583
Tjelvar Guo <tjegu689@student.liu.se>
2019-02-22 15:01:30 +0100

    save: add loginscreen

--- NununuCompany/index.js
+++ NununuCompany/index.js
@@ -3,5 +3,6 @@
 import {AppRegistry} from 'react-native'
 import App from './App'
 import {name as appName} from './app.json'
+import LogInScreen from './screens/LogInScreen'
 
-AppRegistry.registerComponent(appName, () => App)
+AppRegistry.registerComponent(appName, () => LogInScreen)
