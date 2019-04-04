dd25890479efed6b04f04313e68f717eacae0169
Richard Almgren Friberg <rical803@student.liu.se>
2019-04-04 10:21:17 +0200

    swish number nullable

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,7 +11,7 @@ class Company(db.Model):
     __tablename__ = 'company'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), unique=True, nullable=False)
-    swish_number = db.Column(db.Integer, nullable=False)
+    swish_number = db.Column(db.Integer)
     reg_date = db.Column(db.DateTime)
     # for now: a company has ONE owner and the user has A company
     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
