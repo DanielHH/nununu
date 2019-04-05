226de12af31445bf265952eb96cc32942789420d
Daniel Herzegh <danhe178@student.liu.se>
2019-04-05 15:45:59 +0200

    port complete

--- NununuCompany/index.js
+++ NununuCompany/index.js
@@ -3,6 +3,5 @@
 import {AppRegistry} from 'react-native'
 import App from './App'
 import {name as appName} from './app.json'
-import ForgotPswScreen from './screens/ForgotPswScreen'
 
-AppRegistry.registerComponent(appName, () => ForgotPswScreen)
+AppRegistry.registerComponent(appName, () => App)
