2993b604aa438e6e8516fba22b1ef136222164b8
Tjelvar Guo <tjegu689@student.liu.se>
2019-05-02 14:15:51 +0200

    remove default get index from server

--- backend/server.py
+++ backend/server.py
@@ -22,10 +22,6 @@ def verify_token(func):
         return func(*args, **kwargs)
     return wrapper
 
-@app.route('/',methods=["GET"])
-def hello_world():
-    return 'Hello, World!'
-
 
 @app.route("/user/sign-up", methods=['POST'])
 def sign_up():
