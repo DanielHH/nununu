5883cc3bf897646f5be652e9e3f4702310357caf
Tjelvar Guo <tjegu689@student.liu.se>
2019-05-14 14:17:28 +0200

    add tjelvar ipv4 adress

--- Nununu/constants.js
+++ Nununu/constants.js
@@ -1,4 +1,5 @@
 export default {
   EmulatorUrl: 'http://10.0.2.2:5000',
-  DanielIpV4: 'http://192.168.1.2:5000'
+  DanielIpV4: 'http://192.168.1.2:5000',
+  TjelvarIpV4: 'http://192.168.1.173:5000'
 }
