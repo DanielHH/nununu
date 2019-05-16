4a0a07621b0c24c83899518565148e5e78a55a79
Richard Almgren Friberg <rical803@student.liu.se>
2019-05-16 14:01:04 +0200

    Added address to server in constants.js

--- Nununu/constants.js
+++ Nununu/constants.js
@@ -1,5 +1,7 @@
 export default {
   EmulatorUrl: 'http://10.0.2.2:5000',
   DanielIpV4: 'http://192.168.1.2:5000',
-  TjelvarIpV4: 'http://192.168.1.173:5000'
+  TjelvarIpV4: 'http://192.168.1.173:5000',
+  RichardIpv4: 'http://192.168.1.2:5000',
+  ServerURL: 'https://swish.mastega.nu',
 }
