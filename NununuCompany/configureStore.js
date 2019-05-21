a0df3fc2a81e625ab07008c111323c9bd85c3f28
Richard Almgren Friberg <rical803@student.liu.se>
2019-05-21 10:54:12 +0200

    Add redux-websocket middleware

--- NununuCompany/configureStore.js
+++ NununuCompany/configureStore.js
@@ -3,17 +3,20 @@ import { persistStore, persistCombineReducers } from 'redux-persist'
 import thunk from 'redux-thunk'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
 import { reducers } from './redux/reducers'
+import reduxWebsocket from '@giantmachines/redux-websocket'
 
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['order', 'authentication'], 
+  whitelist: ['order', 'authentication'],
 }
 
 const persistedReducer = persistCombineReducers(persistConfig, reducers)
 
+const reduxWebsocketMiddleware = reduxWebsocket()
+
 export default function testExport () {
-  let store = createStore(persistedReducer, applyMiddleware(thunk))
+  let store = createStore(persistedReducer, applyMiddleware(thunk, reduxWebsocketMiddleware))
   // subscribe to store for debbuging, logs all the changes
   // note that logging all the changes is performance demanding
 
